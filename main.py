10936549e2c6e3e9c8e4851d07818d2d079f205d
“shota-08” <shota.kikko.bg@ambl.co.jp>
2024-05-28 00:20:16 +0900

    logging実装

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import uvicorn
 from fastapi import FastAPI
@@ -13,12 +14,18 @@ from llm import llm_engine
 __import__("pysqlite3")
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
+# ロギングの設定
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 # 初期化
 embedding = OpenAIEmbeddings(model= "text-embedding-3-small")
+logger.info("Embedding model initialized")
 
 # chroma db呼び出し
 persist_directory = "./docs/chroma"
 db = Chroma(collection_name="langchain_store", persist_directory=persist_directory, embedding_function=embedding)
+logger.info("Chroma DB initialized with embedding function")
 
 class RequestData(BaseModel):
     text: str
@@ -30,12 +37,15 @@ class LLMResponse(BaseModel):
     content: str
 
 def ask_question(query: str) -> str:
+    logger.info(f"Received query: {query}")
     docs = db.similarity_search(query, k=2)
+    logger.info(f"Documents retrieved: {docs}")
     answer_meta = docs[0].metadata
     answer_title = answer_meta['title']
     answer_url = answer_meta['url']
     answer_content = docs[0].page_content
     answer = llm_engine.get_llm_answer(query, answer_title, answer_content)
+    logger.info(f"Generated answer: {answer}")
     return answer, answer_title, answer_url, answer_content
 
 app = FastAPI()
